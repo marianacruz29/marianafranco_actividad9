37cf483c7c1173c8926a8792acab20a0f08b000d
marianacruz29 <mariana.cruzfr@gmail.com>
2020-11-30 23:27:48 -0600

    actividad9

---
+++ funciones.js
@@ -0,0 +1,12 @@
+console.log("Primera Practica");
+var variableVar ="Variables con Var";
+let variableLet ="Variable con Let";
+const variableConst="Variable con Const";
+
+console.log("variableVar");
+console.log("variableLet");
+console.log("variableConst");
+
+var variableVar="variable con Var";
+
+variableConst="Nuevo Valor de Variable Const";
